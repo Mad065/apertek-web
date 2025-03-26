8b7b65be18461151f819bf2bbd3d3d22314a192d
Mad0 <jose.antonio.pepe@icloud.com>
2025-03-25 19:07:27 -0600

    change hamburger menu to sidebar menu

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,36 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".hamburguer-menu");
-    const navLinks = document.querySelector(".nav-links");
+    const menuIcon = document.querySelector(".icon_menu");
+    const closeMenu = document.querySelector(".close-menu");
+    const sidebar = document.querySelector(".sidebar");
 
     menuIcon.addEventListener("click", function () {
-        if (navLinks.classList.contains("show-menu")) {
-            navLinks.classList.remove("show-menu");
-            navLinks.classList.add("hide-menu");
-
-            // Esperar a que termine la animación antes de ocultar el menú
-            setTimeout(() => {
-                navLinks.style.visibility = "hidden";
-            }, 500);
-        } else {
-            navLinks.classList.remove("hide-menu");
-            navLinks.classList.add("show-menu");
-            navLinks.style.visibility = "visible";
-        }
+        sidebar.classList.add("show");
     });
 
-    // Ocultar menú al hacer clic en un enlace
-    document.querySelectorAll(".nav-links a").forEach(link => {
-        link.addEventListener("click", function () {
-            // Verificar si el icono del menú está visible
-            const displayValue = window.getComputedStyle(menuIcon).getPropertyValue("display");
-            if (displayValue === "block") {
-                navLinks.classList.remove("show-menu");
-                navLinks.classList.add("hide-menu");
+    closeMenu.addEventListener("click", function () {
+        sidebar.classList.remove("show");
+    });
 
-                setTimeout(() => {
-                    navLinks.style.visibility = "hidden";
-                }, 500);
-            }
+    // Cerrar el menú al hacer clic en un enlace
+    document.querySelectorAll(".sidebar a").forEach(link => {
+        link.addEventListener("click", function () {
+            sidebar.classList.remove("show");
         });
     });
 });
